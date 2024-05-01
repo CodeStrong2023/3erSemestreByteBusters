2d7f504f0d4681021c43a4391f4056a9cfda02dd
Emiliano Grossi <emigrossi2004@gmail.com>
2024-05-01 10:52:11 -0300

    Clase 2 Python: Abstract y Static

--- Emiliano-3er-sem/Python-3er-sem/Leccion09/Cuadrado.py
+++ Emiliano-3er-sem/Python-3er-sem/Leccion09/Cuadrado.py
@@ -4,7 +4,7 @@ from Color import Color
 class Cuadrado(FiguraGeometrica, Color):
     def __init__(self, lado, color):
     #    super.__init__(lado)
-    FiguraGeometrica.__init__(self, lado, lado)
+        FiguraGeometrica.__init__(self, lado, lado)
     Color.__init__(self, color)
     
 def calcular_area(self):
