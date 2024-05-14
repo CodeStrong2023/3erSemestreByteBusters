eb58cf68b6c9c460d4c76b05bd8f2d33aa723577
Emiliano Grossi <emigrossi2004@gmail.com>
2024-05-13 21:14:17 -0300

    Clase 2 POO Parte 6 Abstract y Static

--- Emiliano_3er_sem/Python-3er-sem/Leccion10/Constantes.py
+++ Emiliano_3er_sem/Python-3er-sem/Leccion10/Constantes.py
@@ -1,4 +1,4 @@
 class Matematicas:
-    PI = 3.1416 # esta es una variable de clase constante
+    PI = 3.1416 # esta es una variable de clase constante.
 
 MI_CONSTANTE = "Esta es una variable constante"
