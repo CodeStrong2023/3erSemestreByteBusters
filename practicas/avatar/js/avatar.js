374a58f0a0807222212de91fb26703330086a310
Dalma Ponce <106714820+dalmaponce@users.noreply.github.com>
2024-06-06 01:42:41 -0300

    Delete practicas directory

--- practicas/avatar/js/avatar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('boton-personaje').addEventListener('click', () => {
-    const personajeSeleccionado = document.querySelector('input[name="personaje"]:checked');
-    if (personajeSeleccionado) {
-        alert(`Has seleccionado a ${personajeSeleccionado.id}`);
-    } else {
-        alert('Por favor, selecciona un personaje.');
-    }
-});
