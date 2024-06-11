0d212053e5a61d79c46d9bfd596063353304f4c6
nikki-021 <naime14paidican@gmail.com>
2024-06-11 19:47:13 -0300

    subo lecciones 1 y 2

---
+++ JavaScript/TSLeccion01/ciclo-do-while.js
@@ -0,0 +1,8 @@
+// do while
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while (conteo < 3)
+console.log("Fin del ciclo do while");
+
---
+++ JavaScript/TSLeccion02/array-o-arreglo.js
@@ -0,0 +1,4 @@
+//Preguntas si es un array o arreglo
+console.log(Array.isArray(autos));
+
+console.log(autos instanceof Array);
