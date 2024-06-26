a42353667e007f972f5674fae60703695b191a0b
Emiliano Grossi <135922136+EmilianoGrossi@users.noreply.github.com>
2024-06-26 10:51:19 -0300

    inicio y sobre nosotros - integradorUTN

---
+++ sobre_nosotros.py
@@ -0,0 +1,10 @@
+
+
+print("********************************************    SOBRE NOSOTROS    ******************************************** \n "
+                +"Byte Busters es una empresa dedicada a la construcción de viviendas con más de 20 años de experiencia. \n"
+                + "Nuestro compromiso es ayudarte a hacer realidad el sueño de tener tu casa propia. \n"
+                + "Nos destacamos por ofrecer financiación en cuotas fijas en pesos, ajustadas a tu presupuesto. \n"
+                + "Contamos con proyectos de construcción en diferentes plazos y una variedad de tipologías de viviendas. \n"
+                + "Trabajamos con materiales tradicionales de alta calidad y ofrecemos el sistema llave en mano. \n"
+                + "Además, nos encargamos de las instalaciones de servicios, y si no tienes un terreno, te ayudamos a conseguirlo. \n"
+                + "****************************************************************************************************************\n\n");
