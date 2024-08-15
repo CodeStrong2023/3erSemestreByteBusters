cf9cc04673f1645d8bfe5743f02451c1762bb25d
Emiliano Grossi <emigrossi2004@gmail.com>
2024-08-15 11:26:44 -0300

    Clase 01 E-commerce Básico Parte 01

---
+++ Emiliano_3er_sem/Js-4to-sem/e-commerce/client/JS/products.js
@@ -0,0 +1,23 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Pera",
+        price: 480,
+        quanty: 1,
+        img: "/Js-4to-sem/e-commerce/client/media/pera.jpg"
+    },
+    {
+        id: 2,
+        productName: "Sandía",
+        price: 690,
+        quanty: 3,
+        img: "/Js-4to-sem/e-commerce/client/media/sandia.jpg"
+    },
+    {
+        id: 3,
+        productName: "Naranja",
+        price: 200,
+        quanty: 1,
+        img: "/Js-4to-sem/e-commerce/client/media/nararnja.jpg"
+    }
+]
